8b86baca2528a064bc5651f9b11f5e33362497de
Rythamo8055 <vishnuvardhanthe8055@gmail.com>
2025-04-20 21:46:51 +0530

    Update route.js TO USE gemini-2.0-flash MODEL FOR HIGHER RATE LIMITS

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -16,7 +16,7 @@ export async function POST(req) {
 	process.env.GOOGLE_GENERATIVE_AI_API_KEY = customKey;
 
 	const result = await streamText({
-		model: google("models/gemini-1.5-pro-001", {
+		model: google("models/gemini-2.0-flash", {
 			safetySettings: [
 				{
 					category: "HARM_CATEGORY_HATE_SPEECH",
